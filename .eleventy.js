a486acca10be971afb783d32758c1742a9a80ccd
ELiu06 <90780860+ELiu06@users.noreply.github.com>
2022-04-26 10:55:11 -0400

    qwertyuio

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
-module.exports = function(eleventyConfig){
-    return{
-        dir: {
-            input: 
-        }
-    }
-}
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassThroughCopy("src/style");
+  eleventyConfig.addPassThroughCopy("src/md");
+  eleventyConfig.addPassThroughCopy("src/media");
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
