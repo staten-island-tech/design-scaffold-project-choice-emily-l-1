8e0a9f78a8c447bcc0deadbaede74ba815ebc35d
ELiu06 <90780860+ELiu06@users.noreply.github.com>
2022-05-24 10:56:01 -0400

    qwerty

--- dog-shopp/.eleventy.js
+++ dog-shopp/.eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style");
   eleventyConfig.addPassthroughCopy("src/md");
   eleventyConfig.addPassthroughCopy("src/media");
+  eleventyConfig.addPassthroughCopy("src/js");
   return {
     dir: {
       input: "src",
